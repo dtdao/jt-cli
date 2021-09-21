c1b45518b0c747528e857ea6fc8c6f0c6974e270
Dong Dao <kwalitycontrol@gmail.com>
2021-09-21 14:18:30 +0900

    Add logic to scrape specific article

--- cmd/getArticle.go
+++ cmd/getArticle.go
@@ -3,9 +3,16 @@ package cmd
 import (
 	"fmt"
 	"github.com/spf13/cobra"
+	"jt-cli/scraper"
+	"log"
+	url2 "net/url"
 )
 
-func init(){
+var (
+	japan_times = "www.japantimes.co.jp"
+)
+
+func init() {
 	rootCmd.AddCommand(getArticleCmd)
 }
 
@@ -14,7 +21,18 @@ var getArticleCmd = &cobra.Command{
 	Short: "Get a specific article by url",
 	Long: `This will get you a specific articles.  So instead getting all articles for a particular day,
 	This will just get you the specific article given a date`,
-	Run: func(cmd *cobra.Command, args []string){
-		fmt.Println(args)
+	Run: func(cmd *cobra.Command, args []string) {
+		for i, url := range args {
+			url, err := url2.ParseRequestURI(url)
+			if err != nil {
+				fmt.Printf("%s, is not a valid url. \n", args[i])
+				continue
+			}
+			if url.Host != japan_times {
+				log.Fatal("You did not provide a valid JapanTimes url")
+			}
+			articleUrl := fmt.Sprintf("https://%s%s", japan_times, url.Path)
+			scraper.ScrapeUrl(articleUrl)
+		}
 	},
 }
